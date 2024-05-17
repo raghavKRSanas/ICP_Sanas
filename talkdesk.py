6177082ef493738b565465b9d3e34ea61205be79
raghavKRSanas <raghav.kr@sanas.ai>
2024-05-17 14:20:34 +0530

    Update talkdesk.py -- adding multiple files

--- talkdesk.py
+++ talkdesk.py
@@ -1,9 +1,11 @@
 import time
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 import json
 import virtualJack as vj
 import os
+
 
 def permissionAccess():
     preferences = {
@@ -14,8 +16,8 @@ def permissionAccess():
     return chrome_options
 
 
-def iframeHandler(driver,title=None,default=None):
-    if title!=None:
+def iframeHandler(driver, title=None, default=None):
+    if title != None:
         iframe = driver.find_elements(By.TAG_NAME, "iframe")
         for frame in iframe:
             if frame.get_attribute("title") == title:
@@ -25,7 +27,8 @@ def iframeHandler(driver,title=None,default=None):
     if default:
         driver.switch_to.default_content()
 
-def dropDownSelection(driver,cssSelector,selectText):
+
+def dropDownSelection(driver, cssSelector, selectText):
     elements = driver.find_elements(By.CSS_SELECTOR, cssSelector)
     for ele in elements:
         if selectText in ele.text:
@@ -38,6 +41,40 @@ def readInputValues(filename):
     return config
 
 
+def callEnd(driver):
+    iframeHandler(driver, title='Conversations')
+    time.sleep(5)
+    driver.find_element(By.XPATH, "//i[normalize-space()='call_end']").click()
+    time.sleep(5)
+    try:
+        driver.find_element(By.XPATH, "//i[normalize-space()='close_outline']").click()
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//span[contains(text(),'Yes, dismiss')]").click()
+    except NoSuchElementException:
+        print("Sumbmit dailogue box was not visible")
+    iframeHandler(driver, default=True)
+    time.sleep(5)
+
+
+def callStart(driver, countryCode, phoneNumber):
+    driver.find_element(By.XPATH, "//button[@aria-label='Conversations']").click()
+    time.sleep(15)
+    iframeHandler(driver, title='Conversations')
+    driver.find_element(By.XPATH, "//span[@class='co-list__item-content'][normalize-space()='+1']").click()
+    time.sleep(2)
+    countryCodeXpath = "//span[normalize-space()='" + countryCode + "']"
+    driver.find_element(By.XPATH, countryCodeXpath).click()
+    driver.find_element(By.XPATH, "//input[@placeholder='Type or paste a number']").send_keys(phoneNumber)
+    driver.find_element(By.XPATH, "//button[@class='react-button_1-3-0_co-button react-button_1-3-0_co-button--primary "
+                                  "react-button_1-3-0_co-button--medium react-button_1-3-0_co-button--full-width']").click()
+    time.sleep(10)
+    conversationID = driver.find_element(By.XPATH, "//span[@data-bi='snapshot-tab-conversation-details-card"
+                                                   "-interaction_id']").text
+    print(conversationID)
+    iframeHandler(driver, default=True)
+    return conversationID
+
+
 #Reading all values from json file
 inputData = readInputValues('variable.json')
 userEmail = inputData['userEmail']
@@ -48,6 +85,8 @@ phoneNumber = inputData['phoneNumber']
 directory_path = inputData['directory_path']
 
 # Configuration setup
+vac_device_id = int(vj.list_sound_devices())
+wav_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.endswith('.wav')]
 chrome_options = permissionAccess()
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://sanasai.talkdeskid.com/login")
@@ -72,33 +111,11 @@ iframeHandler(driver, default=True)
 time.sleep(15)
 
 #Dailer setup
-driver.find_element(By.XPATH, "//button[@aria-label='Conversations']").click()
-time.sleep(15)
-iframeHandler(driver, title='Conversations')
-driver.find_element(By.XPATH, "//span[@class='co-list__item-content'][normalize-space()='+1']").click()
-time.sleep(2)
-countryCodeXpath = "//span[normalize-space()='"+countryCode+"']"
-driver.find_element(By.XPATH, countryCodeXpath).click()
-driver.find_element(By.XPATH, "//input[@placeholder='Type or paste a number']").send_keys(phoneNumber)
-driver.find_element(By.XPATH, "//button[@class='react-button_1-3-0_co-button react-button_1-3-0_co-button--primary "
-                              "react-button_1-3-0_co-button--medium react-button_1-3-0_co-button--full-width']").click()
-iframeHandler(driver, default=True)
-index = vj.list_sound_devices()
-# Get list of WAV files in the directory
-wav_files = [os.path.join(directory_path, file) for file in os.listdir(directory_path) if file.endswith('.wav')]
 
-# Virtual Audio Cable device ID
-vac_device_id = int(index)  # Device ID for "Line 1 (Virtual Audio Cable)" (change this to the appropriate ID)
-time.sleep(15)
-vj.play_audio_files_to_vac(wav_files, vac_device_id)
-print("audio played")
-iframeHandler(driver, title='Conversations')
-time.sleep(5)
-driver.find_element(By.XPATH, "//i[normalize-space()='call_end']").click()
-time.sleep(5)
-driver.find_element(By.XPATH, "//i[normalize-space()='close_outline']").click()
-time.sleep(5)
-driver.find_element(By.XPATH, "//span[contains(text(),'Yes, dismiss')]").click()
-iframeHandler(driver, default=True)
-time.sleep(5)
+for file in wav_files:
+    conversationID = callStart(driver, countryCode, phoneNumber)
+    vj.play_audio_files_to_vac(file, vac_device_id)
+    print("audio played in location {} and conversation ID is {} ".format(file, conversationID))
+    callEnd(driver)
+
 driver.quit()
