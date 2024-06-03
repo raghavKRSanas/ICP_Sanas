545a2d434a2f58cffcfadd793ca6452353c1f089
raghavKRSanas <raghav.kr@sanas.ai>
2024-06-03 19:55:28 +0530

    adding aws

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-import launchApplication as lapp
+from datetime import datetime
+import logging
 
 
 def readInputValues(filename):
@@ -7,20 +8,30 @@ def readInputValues(filename):
         config = json.load(file)
     return config
 
+
 #Reading required values
 inputData = readInputValues('variable2.json')
 appPath = inputData['appLocation']
-dailerTesting = inputData['dailerTesting']
+dialerTesting = inputData['dialerTesting']
 
 #Dailer and respective dailer python file to import and run
 files = {
-    'TalkDesk': 'talkdesk'
+    'TalkDesk': 'talkdesk',
+    '8X8': '8X8',
+    'AWS': 'awsDailer'
 }
 
 
 def main():
-    lapp.launchAPP(appPath)
-    importModule = files[dailerTesting]
+
+    log_filename = datetime.now().strftime('automation_log_%Y%m%d_%H%M%S.log')
+    log_filename = dialerTesting + '_' + log_filename
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        handlers=[logging.FileHandler(log_filename), logging.StreamHandler()])
+
+    logger = logging.getLogger(__name__)
+    importModule = files[dialerTesting]
     dailerPy = __import__(importModule)
     dailerPy.main()
 
