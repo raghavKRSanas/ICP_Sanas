41ca81f95cf8cd9c03fa29fcf419dd68bdbdba1b
raghavKRSanas <raghav.kr@sanas.ai>
2024-06-18 13:35:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ import downloadApp as dapp
 import launchApplication as lapp
 from datetime import datetime
 import logging
+import getConverstionID
+
+
+
 def readInputValues(filename):
     with open(filename, 'r') as file:
         config = json.load(file)
     return config
 
-#Reading required values
-inputData = readInputValues('variable2.json')
+
+# Reading required values
+inputData = readInputValues('variable.json')
 appPath = inputData['appLocation']
 dialerTesting = inputData['dialerTesting']
 url = inputData['url']
-download_dir = inputData['downloadDir']
+downloadDir = inputData['downloadDir']
+downloadDestination = inputData['downloadDestination']
+download = inputData['download']
+appDownloadDir = inputData["appDownloadDir"]
 
-#Dailer and respective dailer python file to import and run
+
+# Dailer and respective dailer python file to import and run
 files = {
     'Talkdesk': 'talkdesk',
     '8X8': '8X8',
@@ -30,14 +39,20 @@ def main():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         handlers=[logging.FileHandler(log_filename), logging.StreamHandler()])
-
     logger = logging.getLogger(__name__)
-    # dapp.downloadApp(url, download_dir)
-    # dapp.installApp(url, download_dir)
-    # lapp.launchAPP(appPath)
+
+    dapp.downloadApp(url, appDownloadDir)
+    dapp.installApp(url, appDownloadDir)
+    lapp.cmdlauchApp(appPath)
     importModule = files[dialerTesting]
     dailerPy = __import__(importModule)
     dailerPy.main()
+    if dialerTesting == 'Talkdesk' and download:
+        #log_filename = "Talkdesk_automation_log_20240617_175244.log"
+        conversationIDDict = getConverstionID.getConversationID(log_filename)
+        import talkdeskDownloader as td
+        td.main(conversationIDDict, downloadDir, downloadDestination)
+
 
 
 if __name__ == "__main__":
