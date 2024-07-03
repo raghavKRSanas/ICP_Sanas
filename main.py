36a8fafa089af0535266e635236b5e81784cf2f0
raghavKRSanas <raghav.kr@sanas.ai>
2024-07-03 18:39:58 +0530

    Update main.py aws

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
+import time
 import downloadApp as dapp
 import launchApplication as lapp
 from datetime import datetime
 import logging
-import getConversationID
-
+import getConverstionID
 
 
 def readInputValues(filename):
@@ -23,7 +23,6 @@ downloadDestination = inputData['downloadDestination']
 download = inputData['download']
 appDownloadDir = inputData["appDownloadDir"]
 
-
 # Dailer and respective dailer python file to import and run
 files = {
     'Talkdesk': 'talkdesk',
@@ -33,7 +32,6 @@ files = {
 
 
 def main():
-
     log_filename = datetime.now().strftime('automation_log_%Y%m%d_%H%M%S.log')
     log_filename = dialerTesting + '_' + log_filename
     logging.basicConfig(level=logging.INFO,
@@ -41,22 +39,39 @@ def main():
                         handlers=[logging.FileHandler(log_filename), logging.StreamHandler()])
     logger = logging.getLogger(__name__)
 
-    dapp.downloadApp(url, appDownloadDir)
-    dapp.installApp(url, appDownloadDir)
-    lapp.cmdlauchApp(appPath)
+    # appVersion = talkdeskDownloader.getAppVersion()
+    # if appVersion:
+    #     downloadAppversion = url.split('/')[-1]
+    #     if appVersion in downloadAppversion:
+    #         logging.info("The app is already installed with same version.")
+    #     else:
+    #         dapp.downloadApp(url, appDownloadDir)
+    #         dapp.installApp(url, appDownloadDir)
+    # else:
+    #     dapp.downloadApp(url, appDownloadDir)
+    #     dapp.installApp(url, appDownloadDir)
+
+
+    #lapp.cmdlauchApp(appPath)
     importModule = files[dialerTesting]
     dailerPy = __import__(importModule)
     dailerPy.main()
     if dialerTesting == 'Talkdesk' and download:
-        #log_filename = "Talkdesk_automation_log_20240617_175244.log"
-        conversationIDDict = getConversationID.getConversationID(log_filename)
+        #Added sleep time since the recording updation takes 30sec in dailer
+        time.sleep(30)
+        #log_filename = "Talkdesk_automation_log_20240618_193928.log"
+        conversationIDDict = getConverstionID.getConversationID(log_filename, dialerTesting)
         import talkdeskDownloader as td
         td.main(conversationIDDict, downloadDir, downloadDestination)
 
+    if dialerTesting == 'AWS' and download:
+        # Added sleep time since the recording update takes 30sec in dailer
+        #time.sleep(30)
+        log_filename = "AWS_automation_log_20240703_165656.log"
+        conversationIDDict = getConverstionID.getConversationID(log_filename, dialerTesting)
+        import awsDownloader as ad
+        ad.main(conversationIDDict, downloadDir, downloadDestination)
 
 
 if __name__ == "__main__":
     main()
-
-
-
