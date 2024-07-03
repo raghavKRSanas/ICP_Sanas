b83399b0e4217b33135f3f3502e6f9734855f147
raghavKRSanas <raghav.kr@sanas.ai>
2024-07-03 16:52:35 +0530

    Update unzipStore.py

--- unzipStore.py
+++ unzipStore.py
@@ -1,12 +1,15 @@
 import os
 import zipfile
 import shutil
-
+import logging
 
 def unzip_latest_folder(source_dir, destination_dir, new_wav_name):
     zip_files = [file for file in os.listdir(source_dir) if file.endswith('.zip')]
     if not zip_files:
-        print("No zip files found in the source directory.")
+        logging.info("Retrying to find the zip folder.")
+        zip_files = [file for file in os.listdir(source_dir) if file.endswith('.zip')]
+        if not zip_files:
+            logging.error("zip folder is found")
         return
     zip_files.sort(key=lambda x: os.path.getmtime(os.path.join(source_dir, x)), reverse=True)
     latest_zip_file = os.path.join(source_dir, zip_files[0])
@@ -14,7 +17,7 @@ def unzip_latest_folder(source_dir, destination_dir, new_wav_name):
         os.makedirs(destination_dir)
     with zipfile.ZipFile(latest_zip_file, 'r') as zip_ref:
         zip_ref.extractall(destination_dir)
-    print(f"Latest zip file '{latest_zip_file}' extracted to '{destination_dir}'.")
+    logging.info("Latest zip file '{}' extracted to '{}'.".format(latest_zip_file, destination_dir))
     mp3_files = [f for f in os.listdir(destination_dir) if f.endswith('.mp3')]
     if mp3_files:
         latest_mp3_file = max(mp3_files, key=lambda x: os.path.getmtime(os.path.join(destination_dir, x)))
@@ -25,13 +28,10 @@ def unzip_latest_folder(source_dir, destination_dir, new_wav_name):
         os.rename(os.path.join(destination_dir, latest_mp3_file), new_file_path)
         print(f"Latest .mp3 file renamed to '{new_wav_name}.mp3' in the location {destination_dir}")
     else:
-        print("No .mp3 files found in the destination directory.")
+        logging.error("No .mp3 files found in the destination directory.")
 
 
 # source_directory = "C:\\Users\\RaghavKR\\Downloads\\recordings"
 # destination_directory = "C:\\Users\\RaghavKR\\Downloads\\extracted_recordings"
 # new_wav_name = 'new_name'
 # unzip_latest_folder(source_directory, destination_directory, new_wav_name)
-
-
-
