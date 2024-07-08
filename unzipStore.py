1e4e00012e780d470271102a13e46eeaff6eb8ff
raghavKRSanas <raghav.kr@sanas.ai>
2024-07-08 14:22:53 +0530

    Update unzipStore.py

--- unzipStore.py
+++ unzipStore.py
@@ -3,17 +3,19 @@ import time
 import zipfile
 import shutil
 import logging
+import glob
+import time
 
 
-def unzip_latest_folder(source_dir, destination_dir, new_wav_name):
-    zip_files = [file for file in os.listdir(source_dir) if file.endswith('.zip')]
-    if not zip_files:
+def unzip_latest_folder(source_dir, destination_dir, new_wav_name, allFiles):
+    zip_files = glob.glob(os.path.join(source_dir, '*.zip'))
+    time.sleep(5)
+    latest_zip_file = max(zip_files, key=os.path.getmtime)
+    if latest_zip_file in allFiles:
         logging.info("Retrying to find the zip folder after 5 seconds")
         time.sleep(5)
-        zip_files = [file for file in os.listdir(source_dir) if file.endswith('.zip')]
-        if not zip_files:
-            logging.error("zip folder not found")
-        return
+        latest_zip_file = max(zip_files, key=os.path.getmtime)
+    print("latest file found {} ".format(latest_zip_file))
     zip_files.sort(key=lambda x: os.path.getmtime(os.path.join(source_dir, x)), reverse=True)
     latest_zip_file = os.path.join(source_dir, zip_files[0])
     if not os.path.exists(destination_dir):
@@ -32,6 +34,7 @@ def unzip_latest_folder(source_dir, destination_dir, new_wav_name):
         print(f"Latest .mp3 file renamed to '{new_wav_name}.mp3' in the location {destination_dir}")
     else:
         logging.error("No .mp3 files found in the destination directory.")
+    return latest_zip_file
 
 def rename_and_move_latest_file(source_dir, destination_dir, new_wav_name, fileExt):
     os.makedirs(destination_dir, exist_ok=True)
